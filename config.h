6313b04b20ff3fb311b0720e247f444f017d8017
Brandon Lynch <brandon.lynch2402@gmail.com>
2020-11-30 19:34:18 -0600

    shiftview patch added

--- config.h
+++ config.h
@@ -99,6 +99,7 @@ ResourcePref resources[] = {
 		{ "mfact",      	 	FLOAT,   &mfact },
 };
 
+#include "shiftview.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
@@ -140,6 +141,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,						XK_n,	   shiftview,	   {.i = +1 } },
+	{ MODKEY,						XK_b,	   shiftview,	   {.i = -1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
