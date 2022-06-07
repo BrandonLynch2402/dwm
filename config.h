3004231caf8a2d22b51df94794838e4d44f63afb
Brandon Lynch <brandon.lynch2402@gmail.com>
2022-06-07 10:50:06 -0500

    changed close window key binding

--- config.h
+++ config.h
@@ -129,7 +129,7 @@ static Key keys[] = {
 	/*{ MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },*/
 	/*{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },*/
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,		                XK_q,      killclient,     {0} },
+	{ MODKEY,		                XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_y,      setlayout,      {.v = &layouts[11]} },
 	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[7]} },
